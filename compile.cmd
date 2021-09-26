3288144e6f94caa53bfc1b944cea005a5eedcfd9
phibr0 <bronzel.phillip@gmail.com>
2021-09-26 11:29:47 +0200

    Improve Font compatibility

---
+++ compile.cmd
@@ -0,0 +1,1 @@
+sass .\latex.scss .\latex.css --no-source-map
--- compile.ps1
+++
@@ -1,6 +0,0 @@
-sass .\latex.scss .\latex.css --no-source-map --style compressed
-
-$(
-    (Get-Content comment.txt -Raw)
-    (Get-Content latex.css -Raw)
-) | Out-File latex.css
